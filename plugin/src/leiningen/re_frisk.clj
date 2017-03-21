2cb64d2107e7728069ace48f94e420aca93edede
Andrey Shovkoplyas <motor4ik@gmail.com>
2017-03-21 15:42:16 +0300

    implemented use-re-natal subtask

--- plugin/src/leiningen/re_frisk.clj
+++ plugin/src/leiningen/re_frisk.clj
@@ -1,10 +1,29 @@
 (ns leiningen.re-frisk
-  (:require [leiningen.core.eval :as leval]))
+  (:require [leiningen.core.eval :as leval]
+            [clojure.data.json :as json]
+            [clojure.string :as str]))
+
+(defn- replace-host [txt host]
+  (str/replace txt #"enable-re-frisk-remote!.*\)" (str "enable-re-frisk-remote! {:host \"" host ":4567\"})")))
+
+(defn use-re-natal [project]
+  (let [config (json/read-str (slurp ".re-natal") :key-fn keyword)
+        dev-root (get-in config [:envRoots :dev])
+        android-path (str dev-root "/env/android/main.cljs")
+        ios-path (str dev-root "/env/ios/main.cljs")]
+    (spit android-path (replace-host (slurp android-path) (:androidHost config)))
+    (spit ios-path (replace-host (slurp ios-path) (:iosHost config)))
+    (leiningen.core.main/info (str "re-frisk server for iOS: " (:iosHost config) ":4567"))
+    (leiningen.core.main/info (str "re-frisk server for Android: " (:androidHost config) ":4567"))))
 
 (defn ^:no-project-needed re-frisk
-  [project & [port]]
-  (leval/eval-in-project
-    {:dependencies '[[org.clojure/clojure "1.8.0"]
-                     [re-frisk-sidecar "0.4.4"]]}
-    `(re-frisk-sidecar.core/-main ~port)
-    '(require 're-frisk-sidecar.core)))
+  {:subtasks [#'use-re-natal]}
+  [project & [subtask-or-port]]
+  (case subtask-or-port
+    "use-re-natal" (use-re-natal project)
+    (leval/eval-in-project
+      {:dependencies '[[org.clojure/clojure "1.8.0"]
+                       [re-frisk-sidecar "0.4.4"]]}
+      `(re-frisk-sidecar.core/-main ~subtask-or-port)
+      '(require 're-frisk-sidecar.core))))
+
